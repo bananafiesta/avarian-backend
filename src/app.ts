eb4fea23810146fa737d53fe3289c922d8fc9834
bananafiesta <zhang.vctr@gmail.com>
2024-11-08 02:27:46 -0800

    remove old, unused comments

--- src/app.ts
+++ src/app.ts
@@ -23,16 +23,9 @@ for (const commandName in CommandMap) {
   }
 }
 
-// client.once(Events.ClientReady, readyClient => {
-//   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-// });
 
 for (const event of EventList) {
-  // if (event.once) {
-  //   client.once(event.name, (...args) => event.execute(...args));
-  // } else {
-  //   client.on(event.name, (...args) => event.execute(...args));
-  // }
+
   if ('once' in event) {
     client.once(event.name, (...args: Parameters<typeof event.execute>) => event.execute(...args));
   } else {
@@ -40,26 +33,4 @@ for (const event of EventList) {
   }
 }
 
-
 client.login(token);
-
-// client.on(Events.InteractionCreate, async interaction => {
-//   if (!interaction.isChatInputCommand()) return;
-//   const command = interaction.client.commands.get(interaction.commandName);
-
-//   if (!command) {
-//     console.error(`No command matching ${interaction.commandName} was found.`);
-//     return;
-//   }
-
-//   try {
-//     await command.execute(interaction);
-//   } catch (error) {
-//     console.error(error);
-//     if (interaction.replied || interaction.deferred) {
-//       await interaction.followUp({ context: 'There was an error while executing this command!', ephemeral: true});
-//     } else {
-//       await interaction.reply({content: 'There was an error while executing this command!', ephemeral: true});
-//     }
-//   }
-// });
