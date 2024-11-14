3a23ce2c33b681d139329783c3297bb902d7f2e7
bananafiesta <zhang.vctr@gmail.com>
2024-11-13 23:47:48 -0800

    add xconomy query

--- src/db/auraskillsDb.ts
+++ src/db/auraskillsDb.ts
@@ -1,28 +1,29 @@
 import mysql from 'mysql';
 import 'dotenv/config';
 
-export function queryLeaderboard(): Promise<string> {
+export async function queryLeaderboard(): Promise<string> {
 
-const connection = mysql.createConnection({
-  host: process.env.AURASKILLS_HOST,
-  port: parseInt(process.env.AURASKILLS_PORT),
-  user: process.env.AURASKILLS_USER,
-  password: process.env.AURASKILLS_PASSWORD,
-  database: process.env.AURASKILLS_DB
-  
-});
+  const connection = mysql.createConnection({
+    host: process.env.AURASKILLS_HOST,
+    port: parseInt(process.env.AURASKILLS_PORT),
+    user: process.env.AURASKILLS_USER,
+    password: process.env.AURASKILLS_PASSWORD,
+    database: process.env.AURASKILLS_DB
+    
+  });
 
-connection.connect();
+  connection.connect();
 
-const query = "SELECT users.player_uuid, SUM(skills.skill_level) as total FROM auraskills_skill_levels AS skills INNER JOIN auraskills_users as users ON users.user_id = skills.user_id GROUP BY users.user_id ORDER BY total DESC, SUM(skills.skill_xp) DESC LIMIT 25"
-return new Promise((resolve, reject) => {
-  connection.query(query, (err, rows, fields) => {
-    if (err) {
-      return reject(err.message);
-    }
-    resolve(rows);
+  const query = "SELECT users.player_uuid, SUM(skills.skill_level) as total FROM auraskills_skill_levels AS skills INNER JOIN auraskills_users as users ON users.user_id = skills.user_id GROUP BY users.user_id ORDER BY total DESC, SUM(skills.skill_xp) DESC LIMIT 25"
+  return new Promise((resolve, reject) => {
+    connection.query(query, (err, rows, fields) => {
+      connection.end();
+      if (err) {
+        return reject(err.message);
+      }
+      resolve(rows);
+    })
   })
-})
 // connection.query(query, (err, rows, fields) => {
 //   if (err) {
 
---
+++ src/db/xconomyDb.ts
@@ -0,0 +1,35 @@
+import mysql from 'mysql';
+import 'dotenv/config';
+
+function newConnection() {
+  return mysql.createConnection({
+    host: process.env.XCONOMY_HOST,
+    port: parseInt(process.env.XCONOMY_PORT),
+    user: process.env.XCONOMY_USER,
+    password: process.env.XCONOMY_PASSWORD,
+    database: process.env.XCONOMY_DB
+  })
+}
+
+export async function findUserEconomy(player_uuid: string) {
+  const connection = newConnection();
+  connection.connect();
+  const query = `SELECT balance FROM xconomy WHERE uid = '${player_uuid}'`
+  // connection.query(query, (err, rows, fields) => {
+  //   connection.end();
+  //   if (err) throw err;
+  //   console.log(rows);
+  
+  // })
+  return new Promise((resolve, reject) => {
+    connection.query(query, (err, rows, fields) => {
+      connection.end();
+      if (err) {
+        return reject(err.message);
+      }
+
+      // Should look like [ RowDataPacket { balance: 50 } ]
+      resolve(rows);
+    })
+  })
+}
