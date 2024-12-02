1f4083f7e353f379c250be3e74665b9c18606dbc
bananafiesta <zhang.vctr@gmail.com>
2024-12-02 03:05:10 -0800

    change to subcommand structure

--- src/services/discord/commands/minecraft/whitelist.ts
+++ src/services/discord/commands/minecraft/whitelist.ts
@@ -3,28 +3,54 @@ import { SlashCommandBuilder } from "discord.js";
 export const Whitelist = {
   data: new SlashCommandBuilder()
     .setName('whitelist')
-    .setDescription('Adds a player to the Minecraft server whitelist while associating their Minecraft and Discord accounts.')
-    .addStringOption(option => 
-      option.setName('mode')
+    .setDescription('Controls the whitelist for the Minecraft server.')
+    .addSubcommand(subcommand => 
+      subcommand.setName('add')
+      .setDescription('Add player to the whitelist')
+      .addStringOption(option => 
+        option.setName('mode')
         .setDescription('Whitelist action to take')
-        .setRequired(true)
         .addChoices(
-          { name: 'Add to whitelist', value: 'add' },
-          { name: 'Remove from whitelist', value: 'remove' }
-        ))
-    .addStringOption(option => 
-      option.setName('username')
+          { name: 'Add to whitelist', value: 'add' }
+        )
+      )
+      .addStringOption(option => 
+        option.setName('username')
         .setDescription(`Player's Minecraft username`)
         .setRequired(true)
         .setMinLength(3)
+      )
+      .addUserOption(option => 
+        option.setName('discord user')
+        .setDescription('Discord user to associate with')
+        .setRequired(true)
+      )
     )
-    .addUserOption(option =>
-      option.setName('discord user')
-        .setDescription('Discord user to associate with (only needed for adding)')
-        .setRequired(false)
+    .addSubcommand(subcommand => 
+      subcommand.setName('remove')
+      .setDescription('Remove player from the whitelist')
+      .addStringOption(option => 
+        option.setName('mode')
+        .setDescription('Whitelist action to take')
+        .addChoices(
+          { name: 'Remove from whitelist', value: 'remove' }
+        )
+      )
+      .addStringOption(option => 
+        option.setName('username')
+        .setDescription(`Player's Minecraft username`)
+        .setRequired(true)
+        .setMinLength(3)
+      )
     ),
-  async execute(interaction) {
     
+  async execute(interaction) {
+    const subCommand = interaction.options.getSubcommand();
+    if (subCommand === 'add') {
+
+    } else if (subCommand === 'remove') {
+
+    }
   }
   
 
