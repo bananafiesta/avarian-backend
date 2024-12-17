df26187adc425cf6c2c639a65bca19fe8bdfe227
bananafiesta <zhang.vctr@gmail.com>
2024-12-17 14:53:57 -0800

    finish xconomyDb tests

--- src/db/xconomyDb.test.ts
+++ src/db/xconomyDb.test.ts
@@ -31,6 +31,48 @@ describe('findUsersEconomy', () => {
     expect(mockConnection.connect).toHaveBeenCalled();
     expect(mockConnection.end).toHaveBeenCalled();
     expect(result).toEqual(mockRows);
+  });
 
+  it('should reject if query errors', async () => {
+    const mockError = new Error('Xconomy errored');
+    mockConnection.query.mockImplementation((query, uuids, callback) => {
+      callback(mockError, null, null);
+    });
+    await expect(findUsersEconomy(['uuid1'])).rejects.toBe(mockError.message);
+    expect(mockConnection.connect).toHaveBeenCalled();
+    expect(mockConnection.end).toHaveBeenCalled();
   });
 });
+
+describe('findUserEconomy', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should query for valid player balances successfully', async () => {
+    const mockRow = [
+      {balance: 57}
+    ];
+    mockConnection.query.mockImplementation((query, callback) => {
+      callback(null, mockRow, null);
+    });
+    const result = await findUserEconomy('uuid1');
+    expect(mockConnection.query).toHaveBeenCalledWith(
+      `SELECT balance FROM xconomy WHERE uid = 'uuid1'`,
+      expect.any(Function),
+    );
+    expect(mockConnection.connect).toHaveBeenCalled();
+    expect(mockConnection.end).toHaveBeenCalled();
+    expect(result).toEqual(mockRow);
+  });
+
+  it('should reject if query errors', async () => {
+    const mockError = new Error('Xconomy errored');
+    mockConnection.query.mockImplementation((query, callback) => {
+      callback(mockError, null, null);
+    });
+    await expect(findUserEconomy('uuid1')).rejects.toBe(mockError.message);
+    expect(mockConnection.connect).toHaveBeenCalled();
+    expect(mockConnection.end).toHaveBeenCalled();
+  });
+})
