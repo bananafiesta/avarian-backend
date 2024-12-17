928c4f76f753c87803540bf5caa97e01becc17a5
bananafiesta <zhang.vctr@gmail.com>
2024-12-17 03:41:42 -0800

    add jest test case for successful call of findUsersEconomy

--- src/db/xconomyDb.test.ts
+++ src/db/xconomyDb.test.ts
@@ -1,4 +1,4 @@
-
+import { findUserEconomy, findUsersEconomy } from "./xconomyDb";
 
 const mockConnection = {
   connect: jest.fn(),
@@ -8,3 +8,29 @@ const mockConnection = {
 jest.mock('mysql', () => ({
   createConnection: jest.fn((any) => mockConnection)
 }));
+
+describe('findUsersEconomy', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should query for all wallets successfully', async () => {
+    const mockRows = [
+      {balance: 50, uid: 'uuid1'},
+      {balance: 127, uid: 'uuid2'},
+    ];
+    mockConnection.query.mockImplementation((query, uuids, callback) => {
+      callback(null, mockRows, null);
+    });
+    const result = await findUsersEconomy(['uuid1', 'uuid2']);
+    expect(mockConnection.query).toHaveBeenCalledWith(
+      `SELECT balance, uid FROM xconomy WHERE uid in (?)`,
+      [['uuid1', 'uuid2']],
+      expect.any(Function)
+    );
+    expect(mockConnection.connect).toHaveBeenCalled();
+    expect(mockConnection.end).toHaveBeenCalled();
+    expect(result).toEqual(mockRows);
+
+  });
+});
